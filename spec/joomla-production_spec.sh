470f5aa836094faf415d9c8f857236e21065436e
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-09-14 07:59:20 +0300

    Simplify joomla-production

--- spec/joomla-production_spec.sh
+++ spec/joomla-production_spec.sh
@@ -43,37 +43,12 @@ EOF
 
     When run ./joomla-production -f /tmp/test_configuration.php
     The status should be success
-    The output should include 'Checking Joomla production settings'
+    The output should include 'Checking Joomla! production settings'
     The output should include 'All production settings checks passed!'
 
     # Clean up
     rm -f /tmp/test_configuration.php
     rm -rf /tmp/joomla_test_tmp /tmp/joomla_test_logs
-  End
-
-  It "detects issues with tmp_path and log_path directories that don't exist"
-    # Create a test configuration file with non-existent paths
-    cat > /tmp/test_configuration.php <<'EOF'
-<?php
-class JConfig {
-  public $offline = '0';
-  public $sitename = 'Test Site';
-  public $error_reporting = 'maximum';
-  public $debug = '1';
-  public $sef = '0';
-  public $sef_rewrite = '0';
-  public $tmp_path = '/tmp/non_existent_tmp_dir';
-  public $log_path = '/tmp/non_existent_log_dir';
-}
-EOF
-
-    When run ./joomla-production -f /tmp/test_configuration.php
-    The status should be success
-    The output should include 'Checking Joomla production settings'
-    The output should include 'Found 6 issue(s) that should be addressed for production.'
-
-    # Clean up
-    rm -f /tmp/test_configuration.php
   End
 
   It "fixes issues with --fix option"
@@ -94,16 +69,8 @@ EOF
 
     When run ./joomla-production -f /tmp/test_configuration.php --fix
     The status should be success
-    The output should include 'Checking Joomla production settings'
-    The output should include 'Fixing issues...'
-    The output should include 'Production settings fixed!'
-
-    # Verify the settings were fixed
-    tmp_path="$(./joomla-config -f /tmp/test_configuration.php tmp_path)"
-    The value "${tmp_path}" should equal 'tmp'
-
-    log_path="$(./joomla-config -f /tmp/test_configuration.php log_path)"
-    The value "${log_path}" should equal 'administrator/logs'
+    The output should include 'Checking Joomla! production settings'
+    The output should include 'Fixed 4 issue(s).'
 
     # Clean up
     rm -f /tmp/test_configuration.php
